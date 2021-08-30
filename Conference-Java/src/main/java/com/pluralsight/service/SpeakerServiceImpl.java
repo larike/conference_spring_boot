27a74db8908c132cd87f6a1a115f601ed4bb30ad
Lari <35814522+larike@users.noreply.github.com>
2021-08-30 15:55:11 +0300

    Add files via upload

---
+++ Conference-Java/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -0,0 +1,45 @@
+package com.pluralsight.service;
+
+import com.pluralsight.model.Speaker;
+import com.pluralsight.repository.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+//to use @profile: Application hissi-valikosta edit Configurations:
+// VM options: -Dspring.profiles.active=dev
+//@Profile("dev")
+@Component("speakerService")
+public class SpeakerServiceImpl implements SpeakerService {
+
+    private SpeakerRepository repository;
+
+    public SpeakerServiceImpl() {
+        System.out.println("SpeakerServiceImpl no args constructor");
+    }
+
+    public SpeakerServiceImpl(SpeakerRepository speakerRepository) {
+
+        System.out.println("SpeakerServiceImpl repository constructor");
+        repository = speakerRepository;
+    }
+
+    @PostConstruct
+    private void initialize() {
+        System.out.println("We're called after the constructors");
+    }
+
+    public List<Speaker> findAll() {
+        return repository.findAll();
+    }
+
+    @Autowired
+    public void setRepository(SpeakerRepository repository) {
+        System.out.println("SpeakerServiceImpl setter");
+        this.repository = repository;
+    }
+}
+
+
